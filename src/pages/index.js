042214b03db7521363c0cd27e7c4e2a608135ffb
Shy Ruparel <shy@Zi-O.local>
2020-02-24 13:04:35 -0500

    Added time picker, basic other shit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 
+import { DateInput, TimePicker } from "@blueprintjs/datetime";
+import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
+
+const jsDateFormatter = {
+  // note that the native implementation of Date functions differs between browsers
+  formatDate: date => date.toLocaleDateString(),
+  parseDate: str => new Date(str),
+  placeholder: "M/D/YYYY"
+};
+
+const jsTimeFormatter = {
+  useAmPm: "True"
+};
+
 const Index = () => {
   return (
     <header className="App-header">
-      <a href="/cal2text">Pick a date</a>
+      <div>Pick a date between</div>
+      <DateInput {...jsDateFormatter} />
+      <div>to</div>
+      <DateInput {...jsDateFormatter} />
+      <div>between</div>
+      <TimePicker {...jsTimeFormatter} />
+      <div>and</div>
+      <TimePicker {...jsTimeFormatter}/>
     </header>
   );
 };
 
 export default Index;
+
+
