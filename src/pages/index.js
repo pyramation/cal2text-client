46a9dc19a38b6fb86a4e6120548e2882e7239aa5
Shy Ruparel <shy@Zi-O.local>
2020-02-24 16:15:09 -0500

    Styling and stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from "react";
 
 import { DateInput, TimePicker } from "@blueprintjs/datetime";
-import { Button, Icon } from "@blueprintjs/core";
+import { Button, Icon, NumericInput } from "@blueprintjs/core";
 
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
-
 
 const jsDateFormatterStart = {
   // note that the native implementation of Date functions differs between browsers
@@ -20,27 +19,41 @@ const jsDateFormatterEnd = {
   placeholder: "End Date"
 };
 
-const jsTimeFormatter = {
-  useAmPm: "True",
-  showArrowButtons: "True"
+const jsTimeFormatterStart = {
+    useAmPm: "True",
+    showArrowButtons: "True",
+    defaultValue: new Date(2020, 5, 5, 9)
 };
+
+const jsTimeFormatterEnd = {
+  useAmPm: "True",
+  showArrowButtons: "True",
+  defaultValue: new Date(2020, 5, 5, 17, 0)
+};
+
+const jsNumericInputFormatter={
+    allowNumericCharactersOnly: "True",
+    min: 1,
+    value: 1,
+}
 
 const Index = () => {
   return (
     <header className="App-header">
-        <div>Pick a date between</div>
+        <div>Find my free time between</div>
         <div class="no-break">
-            <DateInput {...jsDateFormatterStart} />
-            <div> to </div>
-            <DateInput {...jsDateFormatterEnd} />
-        </div>
-        <div>between</div>
-        <div class="no-break">
-            <TimePicker {...jsTimeFormatter} />
+            <TimePicker {...jsTimeFormatterStart} />
             <div class="vertical-center">and</div>
-            <TimePicker {...jsTimeFormatter}/>
+            <TimePicker {...jsTimeFormatterEnd}/>
         </div>
-      <Button icon="user" text="Get Times" />
+        <div class="no-break">
+            <div> for the next </div>
+            <NumericInput {...jsNumericInputFormatter} />
+            <div> weeks.</div>
+        </div>
+        <div class="no-break">
+            <Button icon="user" text="Get Times" />
+        </div>
     </header>
 
   );
