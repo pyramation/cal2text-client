e774ac608a98659a3a17affd507f28160a69cee3
Shy Ruparel <shy@Zi-O.local>
2020-02-24 15:37:41 -0500

    CSS nonsense

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,18 @@ import { Button, Icon } from "@blueprintjs/core";
 import "@blueprintjs/datetime/lib/css/blueprint-datetime.css";
 
 
-const jsDateFormatter = {
+const jsDateFormatterStart = {
   // note that the native implementation of Date functions differs between browsers
   formatDate: date => date.toLocaleDateString(),
   parseDate: str => new Date(str),
-  placeholder: "M/D/YYYY"
+  placeholder: "Start Date"
+};
+
+const jsDateFormatterEnd = {
+  // note that the native implementation of Date functions differs between browsers
+  formatDate: date => date.toLocaleDateString(),
+  parseDate: str => new Date(str),
+  placeholder: "End Date"
 };
 
 const jsTimeFormatter = {
@@ -21,17 +28,21 @@ const jsTimeFormatter = {
 const Index = () => {
   return (
     <header className="App-header">
-      <div>Pick a date between</div>
-      <DateInput {...jsDateFormatter} />
-      <div>to</div>
-      <DateInput {...jsDateFormatter} />
-      <div>between</div>
-      <TimePicker {...jsTimeFormatter} />
-      <div>and</div>
-      <TimePicker {...jsTimeFormatter}/>
-
+        <div>Pick a date between</div>
+        <div class="no-break">
+            <DateInput {...jsDateFormatterStart} />
+            <div> to </div>
+            <DateInput {...jsDateFormatterEnd} />
+        </div>
+        <div>between</div>
+        <div class="no-break">
+            <TimePicker {...jsTimeFormatter} />
+            <div class="vertical-center">and</div>
+            <TimePicker {...jsTimeFormatter}/>
+        </div>
       <Button icon="user" text="Get Times" />
     </header>
+
   );
 };
 
