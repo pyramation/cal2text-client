afb775a2b31f29ef252488c2830f92f0f079c40b
Matthew Bunday <mkbunday@gmail.com>
2020-02-25 18:37:15 -0500

    Add landing example

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,6 +10,13 @@ const Landing = ({ signIn }) => {
         Get available times in human readable text without sending impersonal
         calendar links
       </h2>
+      <pre className="bp3-code-block">
+        {"I'm free the following times (EST)\n" +
+          "* Wed 26th: 9am to 11:30am, 12:30pm\n" +
+          "to 3:30pm, 4pm to 5pm\n" +
+          "* Thu 27th: 9am to 11:30am, 12pm to 5pm\n" +
+          "* Fri 28th: 9am to 9am, 1:27pm to 5pm\n"}
+      </pre>
       <Button onClick={signIn} intent={"primary"} large>
         Start Scheduling Now
       </Button>
