da46a3daa50b4d97423b717bce755ccfe9ce69ad
Matthew Bunday <mkbunday@gmail.com>
2020-02-25 13:21:30 -0500

    Update results display

--- src/components/ResultsDisplay.js
+++ src/components/ResultsDisplay.js
@@ -3,7 +3,7 @@ import React from "react";
 const ResultsDisplay = ({ results, resultsFetching }) => {
   return results ? (
     <>
-      <div>Final Result: </div>
+      <div>I'm free the following times: </div>
       <p>
         {results.map((daySummary, i) => (
           <li key={i}>{daySummary}</li>
