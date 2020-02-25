7a87d551ce749d1cc0686bb2322155d4c6220ad3
Matthew Bunday <mkbunday@gmail.com>
2020-02-25 18:41:08 -0500

    Add privacy policy

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,9 +17,13 @@ const Layout = ({
         signedIn={signedIn}
         setCalendarsChosen={setCalendarsChosen}
         calendarsChosen={calendarsChosen}
-
       />
       <div className="app-body">{children}</div>
+      <div className="footer">
+        <a className="footer-links" href="/privacy">
+          Privacy Policy
+        </a>
+      </div>
     </div>
   );
 };
