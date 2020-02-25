56b38b021f2c7410775c35a2a604398993fca76e
Matthew Bunday <mkbunday@gmail.com>
2020-02-25 18:44:42 -0500

    Add header link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,56 @@
-
 import React from "react";
 
 import {
-    Alignment,
-    Button,
-    Classes,
-    Navbar,
-    NavbarGroup,
+  Alignment,
+  Button,
+  Classes,
+  Navbar,
+  NavbarGroup
 } from "@blueprintjs/core";
 
 export const Header = ({
-    signOut,
-    signIn,
-    signedIn,
-    setCalendarsChosen,
-    calendarsChosen
+  signOut,
+  signIn,
+  signedIn,
+  setCalendarsChosen,
+  calendarsChosen
 }) => {
+  const notFinishedChoosingCalendars = () => {
+    setCalendarsChosen(false);
+  };
 
-    const notFinishedChoosingCalendars = () => {
-        setCalendarsChosen(false);
-    };
-
-    return (<Navbar>
-        <NavbarGroup align={Alignment.LEFT}>
-            <Button className={Classes.MINIMAL} text="Calendar Management" />
-        </NavbarGroup>
-        <NavbarGroup align={Alignment.RIGHT}>
-
-            {signedIn ? (<>
-                <Button className={Classes.MINIMAL} icon="calendar" text="Select Calendars" onClick={notFinishedChoosingCalendars} />
-                <Button className={Classes.MINIMAL} text="Sign Out" icon="log-out" onClick={signOut} />
-            </>)
-                :
-                (<Button className={Classes.MINIMAL} text="Sign In" icon="log-in" onClick={signIn} />)}
-        </NavbarGroup>
-    </Navbar>);
-}
+  return (
+    <Navbar>
+      <NavbarGroup align={Alignment.LEFT}>
+        <a href="/">
+          <Button className={Classes.MINIMAL} text="Calendar Management" />
+        </a>
+      </NavbarGroup>
+      <NavbarGroup align={Alignment.RIGHT}>
+        {signedIn ? (
+          <>
+            <Button
+              className={Classes.MINIMAL}
+              icon="calendar"
+              text="Select Calendars"
+              onClick={notFinishedChoosingCalendars}
+            />
+            <Button
+              className={Classes.MINIMAL}
+              text="Sign Out"
+              icon="log-out"
+              onClick={signOut}
+            />
+          </>
+        ) : (
+          <Button
+            className={Classes.MINIMAL}
+            text="Sign In"
+            icon="log-in"
+            onClick={signIn}
+          />
+        )}
+      </NavbarGroup>
+    </Navbar>
+  );
+};
